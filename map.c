8e6d6fd67327514a1bf3674a53e6e838dedb35ee
Bus000 <magnus@stavngaard.dk>
2015-06-08 22:55:27 +0200

    Added function to save as a png image
    Added a new function to save a map as a png image to visualize the data.

--- map.c
+++ map.c
@@ -3,12 +3,31 @@
 #include <assert.h>
 #include <time.h>
 #include <stdarg.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <png.h>
+
+/* Help structs. */
+typedef struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+} pixel_t;
+
+typedef struct  {
+    pixel_t *pixels;
+    size_t width;
+    size_t height;
+} bitmap_t;
 
 /* Help functions. */
 static void map_calculate_height(map_t *m, int lower_left_x, int lower_left_y,
         int upper_right_x, int upper_right_y, int random_range);
 static int random_pm_range(int range);
 static int average(int n_args, ...);
+static int pix(int value, int max);
+static int save_png_to_file(bitmap_t *bitmap, const char *path);
+static pixel_t * pixel_at(bitmap_t *bitmap, int x, int y);
 
 void map_init(map_t *m, int length, int width, int random_range)
 {
@@ -36,7 +55,7 @@ void map_init(map_t *m, int length, int width, int random_range)
     srand(time(NULL));
 }
 
-int map_get_height(map_t *m, int x, int y)
+int map_get_height(map_t const *m, int x, int y)
 {
     return m->height[x][y];
 }
@@ -56,6 +75,35 @@ void map_square_diamond(map_t *m)
     m->height[0][m->length-1] = random_pm_range(m->random_range);
 
     map_calculate_height(m, 0, 0, m->width-1, m->length-1, m->random_range);
+}
+
+void save_as_png(map_t const *m, char const *filename)
+{
+    bitmap_t bitmap;
+    int x;
+    int y;
+    int height;
+    pixel_t *pixel;
+
+    /* Create an image. */
+
+    bitmap.width = m->length;
+    bitmap.height = m->width;
+
+    bitmap.pixels = calloc(sizeof(pixel_t), bitmap.width * bitmap.height);
+
+    for (y = 0; y < bitmap.height; y++) {
+        for (x = 0; x < bitmap.width; x++) {
+            height = map_get_height(m, x, y) + m->random_range;
+
+            pixel = pixel_at(&bitmap, x, y);
+            pixel->red = pix(height, m->random_range * 2);
+            pixel->green = pix(height, m->random_range * 2);
+            pixel->blue = pix(height, m->random_range * 2);
+        }
+    }
+
+    save_png_to_file(&bitmap, filename);
 }
 
 /* Function assumes the corner points given is already computed.  It then splits
@@ -130,4 +178,80 @@ static int average(int n_args, ...)
     va_end(ap);
 
     return sum / n_args;
+}
+
+/* http://www.lemoda.net/c/write-png/ */
+static int save_png_to_file(bitmap_t *bitmap, const char *path)
+{
+    FILE * fp;
+    png_structp png_ptr = NULL;
+    png_infop info_ptr = NULL;
+    size_t x, y;
+    png_byte ** row_pointers = NULL;
+    int status = -1;
+    int pixel_size = 3;
+    int depth = 8;
+
+    fp = fopen (path, "wb");
+    if (! fp)
+        goto fopen_failed;
+
+    png_ptr = png_create_write_struct (PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (png_ptr == NULL)
+        goto png_create_write_struct_failed;
+
+    info_ptr = png_create_info_struct (png_ptr);
+    if (info_ptr == NULL)
+        goto png_create_info_struct_failed;
+
+    if (setjmp (png_jmpbuf (png_ptr)))
+        goto png_failure;
+
+    png_set_IHDR (png_ptr, info_ptr, bitmap->width, bitmap->height, depth,
+            PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+            PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+
+    row_pointers = png_malloc (png_ptr, bitmap->height * sizeof (png_byte *));
+    for (y = 0; y < bitmap->height; ++y) {
+        png_byte *row =
+            png_malloc (png_ptr, sizeof (uint8_t) * bitmap->width * pixel_size);
+        row_pointers[y] = row;
+        for (x = 0; x < bitmap->width; ++x) {
+            pixel_t * pixel = pixel_at (bitmap, x, y);
+            *row++ = pixel->red;
+            *row++ = pixel->green;
+            *row++ = pixel->blue;
+        }
+    }
+
+    png_init_io (png_ptr, fp);
+    png_set_rows (png_ptr, info_ptr, row_pointers);
+    png_write_png (png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+
+    status = 0;
+
+    for (y = 0; y < bitmap->height; y++) {
+            png_free (png_ptr, row_pointers[y]);
+        }
+    png_free (png_ptr, row_pointers);
+
+ png_failure:
+ png_create_info_struct_failed:
+    png_destroy_write_struct (&png_ptr, &info_ptr);
+ png_create_write_struct_failed:
+    fclose (fp);
+ fopen_failed:
+    return status;
+}
+
+static int pix(int value, int max)
+{
+    if (value < 0)
+        return 0;
+    return (int) (256.0 *((double) (value)/(double) max));
+}
+
+static pixel_t * pixel_at(bitmap_t *bitmap, int x, int y)
+{
+    return bitmap->pixels + bitmap->width * y + x;
 }
