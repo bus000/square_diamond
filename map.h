872c3f8fe8b454ee4490f504dbc0e6ad20d93c24
Bus000 <magnus@stavngaard.dk>
2015-06-10 16:09:43 +0200

    Added function and macro
    Added the prototype of a function that change the value of a point on
    the map and a macro computing the average of two numbers.

--- map.h
+++ map.h
@@ -9,12 +9,17 @@ typedef struct {
     int water_height; /* The height of any water on the map. */
 } map_t;
 
+#define AVERAGE(x, y) ((x + y) / 2)
+
 /* Initializes the map, the higher the random range, the more hilly the terrain
  * will be. */
 void map_init(map_t *m, int length, int width, int random_range);
 
 /* Returns the height of the map in a specific point. */
 int map_get_height(map_t const *m, int x, int y);
+
+/* Set the height of the map in a specific point. */
+void map_set_height(map_t *m, int x, int y, int value);
 
 /* Set a height for water. */
 void map_set_water_height(map_t *m, int new_height);
