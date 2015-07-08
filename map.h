bc1f63bccc56e0ba6251b7857ee7f1c0fead563a
Bus000 <magnus@stavngaard.dk>
2015-07-08 23:39:45 +0200

    Added new types
    Added types to easier represent points on the map.

--- map.h
+++ map.h
@@ -11,6 +11,23 @@ typedef struct {
     size_t max;          /* The maximum height on the map. */
 } map_t;
 
+typedef struct {
+    int x;
+    int y;
+} point_t;
+
+typedef enum {
+    TOP_LEFT,
+    TOP_RIGHT,
+    BOTTOM_LEFT,
+    BOTTOM_RIGHT,
+    MIDDLE,
+    TOP_MIDDLE,
+    LEFT_MIDDLE,
+    RIGHT_MIDDLE,
+    BOTTOM_MIDDLE
+} pos_t;
+
 #define AVERAGE(x, y) ((x + y) / 2)
 
 /* Initializes the map, the higher the random range, the more hilly the terrain
@@ -32,5 +49,8 @@ void map_square_diamond(map_t *m);
 /* Save the map as a png image. */
 int map_save_as_png(map_t const *m, char const *filename, size_t height,
         size_t width);
+
+/* Return the coordinates for a position on the map. */
+point_t map_get_pos(pos_t pos);
 
 #endif
