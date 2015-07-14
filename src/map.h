ee9c2f6ee7e205a52c6f5916ae826ec5e49e3f0e
Bus000 <magnus@stavngaard.dk>
2015-07-14 15:38:31 +0200

    Fixed compiler error
     The compiler complained that there was no definition of function
    map_shallow_cmp when declared as inline in the .h file.

--- src/map.h
+++ src/map.h
@@ -35,6 +35,6 @@ int map_save_as_png(map_t const *m, char const *filename, size_t height,
 
 /* Compare two map_t's by comparing all metadata about the maps. The function
  * does not actually compare all points on the map. */
-inline int map_shallow_cmp(map_t const *m1, map_t const *m2);
+int map_shallow_cmp(map_t const *m1, map_t const *m2);
 
 #endif
