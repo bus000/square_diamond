b55be7c62a8747d85a8e02a48664c812a9af8ded
Bus000 <magnus@stavngaard.dk>
2015-07-25 22:45:35 +0200

    Fixed off by one in getting height
    The return of map height should go all the way to the side of the map
    and not to the maximum value of the z axis which is one smaller.

--- src/map.c
+++ src/map.c
@@ -58,7 +58,7 @@ int map_init(map_t *m, size_t size, double roughness)
 
 int map_get_height(map_t const *m, int x, int y)
 {
-    if (x >= m->max || x < 0 || y >= m->max || y < 0)
+    if (x >= m->side_len || x < 0 || y >= m->side_len || y < 0)
         return -1;
 
     return m->height[x][y];
