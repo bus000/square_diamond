23a64ac2686b9793c67a7b97971ee2ec2d37ef16
Magnus Stavngaard <magnus@stavngaard.dk>
2018-03-15 13:13:16 +0100

    Specify that maps are computed to unboxed form

--- haskell_impl/src/Map.hs
+++ haskell_impl/src/Map.hs
@@ -22,9 +22,9 @@ import qualified Prelude
 -- TODO: Change to doubles using
 -- https://hackage.haskell.org/package/repa-algorithms-3.4.1.2/docs/Data-Array-Repa-Algorithms-Pixel.html.
 
-data HeightMap s = HeightMap (R.Array s R.DIM2 Word.Word8)
+data HeightMap = HeightMap (R.Array R.U R.DIM2 Word.Word8)
 
-createMap :: C.MonadRandom m => Word.Word32 -> m (HeightMap R.D)
+createMap :: C.MonadRandom m => Word.Word32 -> m HeightMap
 createMap !n = do
     [nw, ne, sw, se] <- C.replicateM 4 (C.getRandomR (0, 255))
 
@@ -37,14 +37,15 @@ createMap !n = do
 
         stepSizes = takeWhile (> 1) . Prelude.iterate (`div` 2) $ sideLen - 1
 
-    return . HeightMap . foldl' (flip step) heightMap $ stepSizes
+        finalMap = foldl' (flip step) heightMap $ stepSizes
+
+    HeightMap <$> R.computeP finalMap
   where
     sideLen = (2^(fromIntegral n)) + 1
-    halfLen = sideLen `div` 2
 
     step size = diamondStep size . squareStep size
 
-saveMap :: R.Source s Word.Word8 => HeightMap s -> FilePath -> IO ()
+saveMap :: HeightMap -> FilePath -> IO ()
 saveMap !(HeightMap arr) !path = R.runIL $ do
     image <- R.Grey <$> R.copyP arr
     R.writeImage path image
@@ -93,121 +94,3 @@ squareStep !size !arr = arr'
     (R.Z :. xMax :. yMax) = R.extent arr
 
     halfSize = size `div` 2
-
-
-
-
-
-
-
-
-
-{-step :: (R.Source s e, Integral e, Integral a, C.MonadRandom m,-}
-    {-C.MonadState [Figure a] m, C.Random e)-}
-    {-=> R.Array s R.DIM2 e-}
-    {-{-- ^ Array to perform step on.-}-}
-    {--> m (R.Array R.D R.DIM2 e)-}
-{-step !arr = do-}
-    {-figures <- C.get-}
-    {-C.put $ filter (centerInside (fromIntegral x) (fromIntegral y)) (getAllSubfigures figures)-}
-
-    {-traceShowM $ "running 1 " ++ (show . length . getAllSubfigures $ figures)-}
-
-    {-newValues <- Map.fromList <$> C.mapM f figures-}
-
-    {-let arr' = R.traverse arr id $ \lookup pos ->-}
-            {-maybe (lookup pos) id $ Map.lookup pos newValues-}
-
-    {-traceShowM "running 2"-}
-
-    {-return arr'-}
-  {-where-}
-    {-(R.Z :. x :. y) = R.extent arr-}
-
-    {-f figure = do-}
-        {--- TODO: Should not be hardcoded and should also be negative.-}
-        {-randomChange <- C.getRandomR (0, 10)-}
-
-        {-let corners = map toIndex . figureCorners $ figure-}
-            {-cornerValues = map fromIntegral . map (arr R.!) $ corners :: [Word.Word32]-}
-            {-average = fromIntegral . (`div` 4) . sum $ cornerValues-}
-
-        {-return (toIndex . _center $ figure, average + randomChange)-}
-
-{-data Point a = Point { _x :: !a, _y :: !a }-}
-  {-deriving (Show, Eq, Ord)-}
-
-{-toIndex :: Integral a => Point a -> R.DIM2-}
-{-toIndex !(Point x y) = R.ix2 (fromIntegral x) (fromIntegral y)-}
-
-{-fromIndex :: Integral a => R.DIM2 -> Point a-}
-{-fromIndex !(R.Z :. x :. y) = Point (fromIntegral x) (fromIntegral y)-}
-
-{-data Figure a-}
-    {-= Square { _center :: !(Point a), _sideLen :: !a }-}
-    {-| Diamond { _center :: !(Point a), _sideLen :: !a }-}
-  {-deriving (Show, Eq, Ord)-}
-
-{-figureCorners :: (Num a, Integral a) => Figure a -> [Point a]-}
-{-figureCorners !(Square (Point x y) sideLen) =-}
-    {-[ Point (x - sideLen) (y - sideLen)-}
-    {-, Point (x - sideLen) (y + sideLen)-}
-    {-, Point (x + sideLen) (y - sideLen)-}
-    {-, Point (x + sideLen) (y + sideLen)-}
-    {-]-}
-{-figureCorners !(Diamond (Point x y) sideLen) =-}
-    {-[ Point (x + sideLen) y-}
-    {-, Point (x - sideLen) y-}
-    {-, Point x (y + sideLen)-}
-    {-, Point x (y - sideLen)-}
-    {-]-}
-
-{-getAllSubfigures :: Integral a => [Figure a] -> [Figure a]-}
-{-getAllSubfigures = foldr getSubFigures []-}
-  {-where-}
-    {-getSubFigures !(Square (Point x y) sideLen) xs =-}
-        {-Diamond (Point (x + sideLen) y) sideLen:-}
-        {-Diamond (Point (x - sideLen) y) sideLen:-}
-        {-Diamond (Point x (y + sideLen)) sideLen:-}
-        {-Diamond (Point x (y - sideLen)) sideLen:-}
-        {-xs-}
-    {-getSubFigures !(Diamond (Point x y) sideLen) xs-}
-        {-| halfLen == 0 = xs-}
-        {-| otherwise =-}
-            {-Square (Point (x - halfLen) (y - halfLen)) halfLen:-}
-            {-Square (Point (x - halfLen) (y + halfLen)) halfLen:-}
-            {-Square (Point (x + halfLen) (y - halfLen)) halfLen:-}
-            {-Square (Point (x + halfLen) (y + halfLen)) halfLen:-}
-            {-xs-}
-      {-where-}
-        {-halfLen = sideLen `div` 2-}
-
-{-getSubFigures :: Integral a => Figure a -> [Figure a]-}
-{-getSubFigures !(Square (Point x y) sideLen) =-}
-    {-[ Diamond (Point (x + sideLen) y) sideLen-}
-    {-, Diamond (Point (x - sideLen) y) sideLen-}
-    {-, Diamond (Point x (y + sideLen)) sideLen-}
-    {-, Diamond (Point x (y - sideLen)) sideLen-}
-    {-]-}
-{-getSubFigures !(Diamond (Point x y) sideLen)-}
-    {-| halfLen == 0 = []-}
-    {-| otherwise =-}
-        {-[ Square (Point (x - halfLen) (y - halfLen)) halfLen-}
-        {-, Square (Point (x - halfLen) (y + halfLen)) halfLen-}
-        {-, Square (Point (x + halfLen) (y - halfLen)) halfLen-}
-        {-, Square (Point (x + halfLen) (y + halfLen)) halfLen-}
-        {-]-}
-  {-where-}
-    {-halfLen = sideLen `div` 2-}
-
-{-centerInside :: (Ord a, Num a) => a -> a -> Figure a -> Bool-}
-{-centerInside x y figure = px >= 0 && py >= 0 && px < x && py < y-}
-  {-where-}
-    {-Point px py = _center figure-}
-
-iterateWhile :: Monad m => (m Bool) -> (a -> m a) -> a -> m a
-iterateWhile !p !f !base = do
-    x <- p
-    if x
-        then f base >>= iterateWhile p f
-        else return base
